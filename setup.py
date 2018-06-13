60d08a99121ea990f5aa85d1de816117258ff4fd
Sanaz Bahargam <sanazb@users.noreply.github.com>
2018-06-12 20:50:12 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
     packages=[
         'rnn'
     ],
-    package_dir={'rnn': 'rnn'},
     include_package_data=True,
     install_requires=requirements,
     license="Apache Software License 2.0",
